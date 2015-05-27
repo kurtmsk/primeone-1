0052352f3d35103093e7cfcace5498958d5b88c8
Matthew Vasseur <mattvasseur@gmail.com>
2015-05-27 10:04:37 -0400

    drop, create, migrate

--- db/schema.rb
+++ db/schema.rb
@@ -29,17 +29,23 @@ ActiveRecord::Schema.define(version: 20150526060651) do
   add_index "autos", ["policy_id"], name: "index_autos_on_policy_id", using: :btree
 
   create_table "brokers", force: :cascade do |t|
-    t.string   "name",        default: ""
-    t.string   "agency_name", default: ""
-    t.string   "email",       default: ""
-    t.string   "phone",       default: ""
-    t.text     "notes",       default: ""
-    t.string   "street",      default: ""
-    t.string   "city",        default: ""
-    t.string   "state",       default: ""
-    t.string   "zip",         default: ""
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "name",                  default: ""
+    t.string   "dba",                   default: ""
+    t.string   "contact_name",          default: ""
+    t.string   "email",                 default: ""
+    t.string   "phone",                 default: ""
+    t.text     "notes",                 default: ""
+    t.integer  "commission",            default: 0
+    t.string   "agreement_sent",        default: ""
+    t.string   "agreement_completed",   default: ""
+    t.string   "pac_risk_fee_schedule", default: ""
+    t.string   "target_premium",        default: ""
+    t.string   "street",                default: ""
+    t.string   "city",                  default: ""
+    t.string   "state",                 default: ""
+    t.string   "zip",                   default: ""
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
   end
 
   add_index "brokers", ["name"], name: "index_brokers_on_name", unique: true, using: :btree
