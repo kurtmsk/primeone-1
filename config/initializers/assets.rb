c47f55201ba925977de9220be720995f10b3e74f
Matthew Vasseur <mattvasseur@gmail.com>
2015-05-29 12:49:13 -0400

    removed mockjax (real jax!!)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -34,7 +34,7 @@ bootstrap/css/bootstrap.min.css )
 # Index Page Assets (broker & policies) (css & js)
 Rails.application.config.assets.precompile += %w( datatables/css/jquery.datatables.css
 datatables/css/jquery.datatables_themeroller.css
-jquery-mockjax-master/jquery.mockjax.js
+
 moment/moment.js
 datatables/js/jquery.datatables.min.js
 pages/table-data.js )
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :policies do
+  resources :policies, except: [:show] do
     member do
       post 'populate', method: :post
     end
