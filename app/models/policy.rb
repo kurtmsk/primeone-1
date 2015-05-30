c6bf43f3b68b212b40f6a4c0c16a3e8cc1bc9ef9
Matthew Vasseur <mattvasseur@gmail.com>
2015-05-30 13:34:59 -0400

    back?

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -4,11 +4,13 @@ class Policy < ActiveRecord::Base
 
   # Associations
   belongs_to :broker
+  has_many :docs, dependent: :destroy
   has_one :property, dependent: :destroy
   has_one :crime, dependent: :destroy
   has_one :gl, dependent: :destroy
   has_one :auto, dependent: :destroy
 
+  accepts_nested_attributes_for :docs
   accepts_nested_attributes_for :property
   accepts_nested_attributes_for :gl
   accepts_nested_attributes_for :crime
---
+++ db/migrate/20150530171716_create_docs.rb
@@ -0,0 +1,19 @@
+class CreateDocs < ActiveRecord::Migration
+  def change
+    create_table :docs do |t|
+      t.string :file, default: ""
+      t.string :form_code, default: ""
+      t.string :description, default: ""
+      t.string :var_1, default: ""
+      t.string :var_2, default: ""
+      t.string :var_3, default: ""
+      t.string :var_4, default: ""
+      t.string :var_5, default: ""
+      t.string :var_6, default: ""
+
+      t.timestamps null: false
+    end
+
+    add_reference :docs, :policy, index: true, foreign_key: true
+  end
+end
